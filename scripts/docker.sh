2b51a795f10ae386391c2b1b412aacf19ff71ac1
Nabil Yermani <nabilyermani@yahoo.de>
2024-09-11 16:48:16 +0200

    Terraform added

---
+++ scripts/docker.sh
@@ -0,0 +1,41 @@
+# Build the backend app container
+docker build \
+    -t feedback-app:v4.0 \
+    -t feedback-app:latest \
+    -t galaataman/feedback-app:v4.0 \
+    -t galaataman/feedback-app:latest .
+
+# Create a docker network for the app
+docker network create feedback-app-nw
+
+# Run the postgres database container
+docker run \
+    --name postgres-db \
+    --network feedback-app-nw \
+    -p 5432:5432 \
+    -e POSTGRES_PASSWORD=password \
+    -e POSTGRES_DB=feedbackdb \
+    -v feedback-app-data:/var/lib/postgresq1/data \
+    -d \
+    --rm \
+    postgres
+
+# Run the backend app container
+docker run \
+    --name feedback-app \
+    --network feedback-app-nw \
+    -p 3030:3000 \
+    -e DB_USER=postgres \
+    -e DB_HOST=postgres-db \
+    -e DB_NAME=feedbackdb \
+    -e DB_PASSWORD=password \
+    -e DB_PORT=5432 \
+    -d \
+    --rm \
+    feedback-app
+
+# Stop the containers
+docker stop feedback-app postgres-db
+
+# Remove the containers
+docker rm feedback-app postgres-db
---
+++ scripts/userdata.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Install Docker
+yum update -y
+yum install -y docker
+
+#Start Docker
+service docker start
+systemctl enable docker
+
+#Install Docker-Compose
+curl -l "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#Download Docker-Compose App Config
+mkdir /home/ec2-user/feedback-app
+cd /home/ec2-user/feedback-app
+wget https://raw.githubusercontent.com/atamankina/feedback-app/main/docker-compose.yml
+
+#Start the app
+sudo docker-compose up -d
